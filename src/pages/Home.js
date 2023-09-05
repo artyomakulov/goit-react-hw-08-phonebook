adb3ed27331bf7134a1b55fc250beb4fd5e1b89f
artyomakulov <artyomakulov@gmail.com>
2023-09-05 17:11:19 +0300

    add new styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,25 @@
-const styles = {
-    container: {
-      minHeight: 'calc(100vh - 50px)',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontWeight: 500,
-      fontSize: 48,
-      textAlign: 'center',
-    },
-  };
-  
-  export default function Home() {
-    return (
-      <div style={styles.container}>
-        <h1 style={styles.title}>
+import React from 'react';
+import css from './Home.module.css';
+import { NavLink } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <>
+      <div className={css.container}>
+        <h1 className={css.title}>
           Phonebook welcome page{' '}
           <span role="img" aria-label="Greeting icon">
             💁‍♀️
           </span>
         </h1>
       </div>
-    );
-  }
+      <p className={css.text}>
+        To use the application, you should be authorized
+      </p>
+      <p className={css.info}>
+        Here you can place any useful information, such as promotions and
+        special offers, social links, demo videos, or instructions
+      </p>
+    </>
+  );
+}
